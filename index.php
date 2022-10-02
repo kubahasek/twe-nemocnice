aeedf650851f0f87dd1e2d20a65c008a784f462f
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-10-02 20:07:19 +0200

    fix: changed link to patient detail

--- index.php
+++ index.php
@@ -65,8 +65,8 @@
       <?php foreach($data as $d): ?>
         <tr class="border border-gray-500">
           <td class="border border-gray-500 p-2"><?= $d["date"] ?></td>
-          <td class="border border-gray-500 p-2"><a href="/patient.php?id=<?= $d["patientId"] ?>" class="text-blue-500 underline"><?= $d["name"] ?></a></td>
-          <td class="border border-gray-500 p-2"><a href="/procedure.php?id=<?= $d["procedureId"] ?>" class="text-blue-500 underline"><?= $d["procedureNum"] ?></a> <?= $d["procedureName"] ?></td>
+          <td class="border border-gray-500 p-2"><a href="detail/patient.php?id=<?= $d["patientId"] ?>" class="text-blue-500 underline"><?= $d["name"] ?></a></td>
+          <td class="border border-gray-500 p-2"><a href="detail/procedure.php?id=<?= $d["procedureId"] ?>" class="text-blue-500 underline"><?= $d["procedureNum"] ?></a> <?= $d["procedureName"] ?></td>
         </tr>
       <?php endforeach; ?>
     </table>
